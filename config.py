8b856bc803200dede80854d180e5b6126ac1aae9
Sainwinian01 <rohitrathor7974@gmail.com>
2025-08-10 09:25:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # basic bro
 import os
 
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
+API_ID = int(os.getenv("API_ID", "27946339"))
+API_HASH = os.getenv("API_HASH", "88eea162bc80b3925a2fb6e955a7ffb8")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 UNPLASH_API = os.getenv("UNPLASH_API", "")
